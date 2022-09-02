2e3ef09dca16a18dff65ff392cfcbb8e443135b9
Geoffery <S00198707@mail.itsligo.ie>
2022-09-02 10:33:50 +0100

    Add project files.

---
+++ Bike.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeptemberExam
+{
+    public enum Type
+    {
+        Racing,
+        Regular,
+        Electric
+    }
+    public class Bike
+    {
+        //properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public Type TypeOfBike { get; set; }
+
+        //methods
+        public void AddVat(decimal AddedVat)
+        {
+            AddedVat = Price * 0.23m;
+            Price = Price + AddedVat;
+
+        }
+    }
+
+
+    //methods
+}
