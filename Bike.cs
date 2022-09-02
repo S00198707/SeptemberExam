585a721174864397f84d2aa79c4985a150ebe556
Geoffery <S00198707@mail.itsligo.ie>
2022-09-02 10:59:01 +0100

    db set created and searcher

--- Bike.cs
+++ Bike.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,12 @@ namespace SeptemberExam
             Price = Price + AddedVat;
 
         }
-    }
 
+    }
+     public class BikeData : DbContext
+    {
+        public BikeData() : base("mybikedata") { }
+
+        public DbSet<Bike> Bikes { get; set; }
+    }
 }
